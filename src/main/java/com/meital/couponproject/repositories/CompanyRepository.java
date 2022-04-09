513c12376d33368cd0eea8a2cb5147c6ba42154f
Meital Mor <meitalmor2@gmail.com>
2022-04-09 15:08:34 +0300

    update repository

--- src/main/java/com/meital/couponproject/repositories/CompanyRepository.java
+++ src/main/java/com/meital/couponproject/repositories/CompanyRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
     boolean existsByEmailAndPassword(String email, String password);
 
     boolean existsById(Long id);
